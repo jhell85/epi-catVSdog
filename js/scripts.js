5736bfcef665d0920e1a1877fbbdcec41e560c98
josh <jhell85@gmail.com>
2020-03-24 16:09:50 -0700

    added styling and remove function for li elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>Kneel before me, peasant.</li>");
-    $("ul#dog").prepend("<li>That's okay. I love you anyway.</li>");    
+    $("ul#dog").prepend("<li>That's okay. I love you anyway.</li>");
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#bark").click(function() {
     $("ul#cat").prepend("<li>I'm still better than you and couldn't be care less.</li>");
     $("ul#dog").prepend("<li>I just met you and you're my best friend.</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#cat").prepend("<li>HISS</li>");
     $("ul#dog").prepend("<li>GROWL</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
 
